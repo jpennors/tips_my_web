6d5eca442d10ae5c394b7d033a4d69c0ed4eed53
Unknown <josselin.pennors@hotmail.fr>
2019-05-05 21:52:18 +0200

    Migrate principle app resources

---
+++ database/migrations/2019_05_03_120222_create_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('name', 40)->unique();
+            $table->uuid('parent_id')->nullable();
+            $table->foreign('parent_id')->references('id')->on('tags');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
---
+++ database/migrations/2019_05_03_120327_create_resources_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('name', 40)->unique();
+            $table->text('description');
+            $table->string('url', 150)->unique();
+            $table->string('image', 100)->nullable();
+            $table->string('language', 5);
+            $table->smallInteger('score')->nullable();
+            $table->integer('visitor')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+
+        Schema::create('resource_tags', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->bigIncrements('id');
+            $table->uuid('resource_id');
+            $table->foreign('resource_id')->references('id')->on('resources');
+            $table->uuid('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->smallInteger('belonging');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+        Schema::dropIfExists('resource_tags');
+    }
+}
