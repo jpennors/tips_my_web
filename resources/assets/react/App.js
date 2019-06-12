9ccdb1d0f41546fb103c5af2fcc65ce0a759524a
Unknown <josselin.pennors@hotmail.fr>
2019-06-12 19:21:12 +0200

    Organise App

--- resources/assets/react/App.js
+++ resources/assets/react/App.js
@@ -1,71 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import HomeScreen from './screens/Home';
-import AdminScreen from './screens/Admin';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import AdminScreen from './screens/AdminScreen';
+import LoginScreen from './screens/LoginScreen';
 
 class App extends React.Component {
-  render() {
-    return (
 
-             <div id="fullpage">
-                <div className="section">
-                    <div className="landing">
-                        <div className="landingWrapper">
-                            <div className="nav">
-                                <a href="index.html"><img src="/assets/images/logo.svg" alt="logo" className="logo" /></a>
-                                <div className="pages">
-                                    <a href="#">Share a website</a>
-                                    <a href="#">About</a>
-                                    <a href="#">Contact</a>
-                                </div>
-                            </div>
-                        </div>
-                        <HomeScreen/>
-                        {/* <Route path="/" component={HomeScreen}/> */}
-                        <div className="footer">
-                            <p>© 2019 TipsMyWeb</p>
-                            <a href="#"><p>Terms</p></a>
-                            <div className="pagesRight">
-                                <a href="#"><img src="/assets/images/Twitter logo.svg" alt="share on twitter logo" className="share" /></a>
-                                <a href="#"><img src="/assets/images/Facebook logo.svg" alt="share on facebook logo" className="share" /></a>
-                                <a href="#"><img src="/assets/images/share.svg" alt="share logo" className="share" /></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    render() {
+        return (
+            <div>
+                <Route exact path="/" component={HomeScreen} />
+                <Route path="/login" component={LoginScreen}/>
+                <Route path="/admin" component={AdminScreen}/>
             </div>
-
-        //     <Route path="/admin" component={AdminScreen}/>
-        //      <div id="fullpage">
-        //         <div className="section">
-        //             <div className="landing">
-        //                 <div className="landingWrapper">
-        //                     <div className="nav">
-        //                         <a href="index.html"><img src="/assets/images/logo.svg" alt="logo" className="logo" /></a>
-        //                         <div className="pages">
-        //                             <a href="#">Share a website</a>
-        //                             <a href="#">About</a>
-        //                             <a href="#">Contact</a>
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //                 <Route path="/" component={HomeScreen}/>
-        //                 <div className="footer">
-        //                     <p>© 2019 TipsMyWeb</p>
-        //                     <a href="#"><p>Terms</p></a>
-        //                     <div className="pagesRight">
-        //                         <a href="#"><img src="/assets/images/Twitter logo.svg" alt="share on twitter logo" className="share" /></a>
-        //                         <a href="#"><img src="/assets/images/Facebook logo.svg" alt="share on facebook logo" className="share" /></a>
-        //                         <a href="#"><img src="/assets/images/share.svg" alt="share logo" className="share" /></a>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </Router>
-    )
-  }
+        )
+    }
 }
 
 export default App;
--- resources/assets/react/index.js
+++ resources/assets/react/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
-// ReactDOM.render(<p>C'estmoi</p>, document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App/>
+    </Router>
+    , document.getElementById('root'));
 
 registerServiceWorker();
