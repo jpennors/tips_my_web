4ffe95d47c734fe8c67b04adb321b957614c91b9
Unknown <josselin.pennors@hotmail.fr>
2019-10-15 23:11:13 +0200

    Add 404 screen

---
+++ resources/assets/react/screens/Error404.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Error404 extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="body-admin">
+                <p>Erreur 404</p>
+            </div>
+        )
+    }
+}
+
+export default Error404;
+
+
+
+
+
+
+
+
+
