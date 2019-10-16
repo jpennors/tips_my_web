82964d617292597706ae906cbe51b9bf2e91f1dd
Unknown <josselin.pennors@hotmail.fr>
2019-10-16 09:55:16 +0200

    Import Interceptor in index file

--- resources/assets/react/index.js
+++ resources/assets/react/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './utils/Interceptor';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
