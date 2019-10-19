7efb1c2ab31cf4a8d924fe9b16152ae185e17d61
lancelotimb <lancelot.imb@gmail.com>
2019-10-19 13:28:35 +0200

    [ESLint] Fix utils

--- resources/assets/react/utils/Interceptor.js
+++ resources/assets/react/utils/Interceptor.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
-import Auth from './Auth';
 
-axios.interceptors.response.use( (response) => {
-    return response;
-}, (error) => {
+axios.interceptors.response.use(response => response, error => {
     // if (error.response.status == 403 || error.response.status == 401) {
     //     Auth.emptyLocalStorage();
     //     Auth.redirectUser();
     // } else {
-        return Promise.reject(error);
-    // }   
+    Promise.reject(error);
+    // }
 });
