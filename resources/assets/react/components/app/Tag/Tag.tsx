af434c5935617e655e99289d1a1409f2ba18b720
lancelotimb <lancelot.imb@gmail.com>
2019-11-03 23:38:33 +0100

    Clean tag classnames

--- resources/assets/react/components/app/Tag/Tag.tsx
+++ resources/assets/react/components/app/Tag/Tag.tsx
@@ -16,7 +16,7 @@ export const Tag: React.FunctionComponent<TagProps> = ({
 }) => (
     <div onClick={onClickCallback}>
         <button
-            className={classNames('tag', 'noselect', {
+            className={classNames('tag', {
                 'tag--selected': isSelected,
             })}
         >
