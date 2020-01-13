6923f5050ef4018451197a3642147fac45d98a53
lancelotimb <lancelot.imb@gmail.com>
2020-01-13 15:47:54 +0100

    Create TS component for header

--- resources/assets/react/screens/HomeScreen.jsx
+++ resources/assets/react/screens/HomeScreen.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { AboutModalContent } from 'tmw/components/app/AboutModalContent';
-import { ContactModalContent } from 'tmw/components/app/ContactModalContent';
-import { HeaderModal } from 'tmw/components/app/HeaderModal';
+import { LayoutHeader } from 'tmw/components/app/LayoutHeader';
 import { ResourceSearch } from 'tmw/components/app/ResourceSearch';
-import { SuggestionModalContent } from 'tmw/components/app/SuggestionModalContent';
 
 class HomeScreen extends Component {
     constructor(props) {
@@ -14,29 +11,13 @@ class HomeScreen extends Component {
         return (
             <div className="landing" id="home">
 
-                {/* Header */}
-                <div className="landing_wrapper">
-                    <div className="nav_landing">
-                        {/* Logo */}
-                        <a href=""><img src="/images/logo.svg" alt="logo" className="logo" /></a>
-                        {/* Lien pour les modals */}
-                        <div className="nav_pages">
-                            <HeaderModal content={<SuggestionModalContent/>} target={<a>Share a website</a>}/>
-                            <HeaderModal content={<AboutModalContent/>} target={<a>About</a>}/>
-                            <HeaderModal content={<ContactModalContent/>} target={<a>Contact</a>}/>
-                        </div>
-                    </div>
-                </div>
+                <LayoutHeader/>
 
-                {/* Home page content, research part */}
                 <ResourceSearch />
 
-                {/* Footer */}
                 <div className="footer">
-                    {/* On left  */}
                     <p>© 2019 TipsMyWeb</p>
                     <a href="#"><p>Terms</p></a>
-                    {/* On right */}
                     <div className="social_medias">
                         <a href="#"><img src="/images/Twitter logo.svg" alt="share on twitter logo" className="social_media_img" /></a>
                         <a href="#"><img src="/images/Facebook logo.svg" alt="share on facebook logo" className="social_media_img" /></a>
