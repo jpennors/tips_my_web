4b7559efdf393015f29363375b83369295dfc91a
lancelotimb <lancelot.imb@gmail.com>
2020-01-13 16:22:32 +0100

    Create TS component for footer

--- resources/assets/react/screens/HomeScreen.jsx
+++ resources/assets/react/screens/HomeScreen.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { LayoutFooter } from 'tmw/components/app/LayoutFooter';
 import { LayoutHeader } from 'tmw/components/app/LayoutHeader';
 import { ResourceSearch } from 'tmw/components/app/ResourceSearch';
 
@@ -10,20 +11,9 @@ class HomeScreen extends Component {
     render() {
         return (
             <div className="landing" id="home">
-
                 <LayoutHeader/>
-
                 <ResourceSearch />
-
-                <div className="footer">
-                    <p>© 2019 TipsMyWeb</p>
-                    <a href="#"><p>Terms</p></a>
-                    <div className="social_medias">
-                        <a href="#"><img src="/images/Twitter logo.svg" alt="share on twitter logo" className="social_media_img" /></a>
-                        <a href="#"><img src="/images/Facebook logo.svg" alt="share on facebook logo" className="social_media_img" /></a>
-                        <a href="#"><img src="/images/Share.svg" alt="share logo" className="social_media_img" /></a>
-                    </div>
-                </div>
+                <LayoutFooter/>
             </div>
         );
     }
