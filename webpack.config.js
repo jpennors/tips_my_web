0320b91e113eff0d77360383ffe8b05ed241ca5b
lancelotimb <lancelot.imb@gmail.com>
2020-01-16 11:05:25 +0100

    Refactor webpack config and add entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
-const path = require("path");
+const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const buildEntryPoint = entryPoint => ([
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    entryPoint,
+]);
+
 module.exports = {
-    entry: [
-        "webpack-dev-server/client?http://localhost:3000",
-        "webpack/hot/only-dev-server",
-        "./resources/assets/react/index.jsx",
-    ],
+    entry: {
+        main: buildEntryPoint('./resources/assets/react/index.jsx'),
+        admin: buildEntryPoint('./resources/assets/react/index.jsx'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].js',
+        // publicPath: '/public/',
+    },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-                options: { presets: ["@babel/preset-env"] },
+                use: ['babel-loader', 'react-hot-loader'],
             },
             {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
-                loader: "ts-loader",
+                use: ['ts-loader'],
             },
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
@@ -40,14 +49,10 @@ module.exports = {
         ],
     },
     resolve: {
-        extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
         alias: {
-            "tmw": __dirname + "/resources/assets/react",
+            'tmw': __dirname + '/resources/assets/react',
         },
-    },
-    output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "js/index.js",
     },
     plugins: [
         new CopyWebpackPlugin([
@@ -56,13 +61,13 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin(),
     ],
     devServer: {
-        contentBase: path.join(__dirname, "public"),
+        contentBase: path.join(__dirname, 'public'),
         headers: { 'Access-Control-Allow-Origin': '*' },
         port: 3000,
         hot: true,
         historyApiFallback: true,
         proxy: {
-            "*": 'http://localhost:8888',
+            '*': 'http://localhost:8888',
         },
     },
 };
