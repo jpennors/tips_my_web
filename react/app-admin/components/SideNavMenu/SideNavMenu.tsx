f3dc13ce2642bd06c140a3b68c4b03aa134065b6
lancelotimb <lancelot.imb@gmail.com>
2020-02-04 01:07:12 +0100

    Refactor side menu

--- react/app-admin/components/SideNavMenu/SideNavMenu.tsx
+++ react/app-admin/components/SideNavMenu/SideNavMenu.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Icon, Menu } from 'semantic-ui-react';
+import { Icon, Menu, StrictMenuProps } from 'semantic-ui-react';
+import { SemanticWIDTHS } from 'semantic-ui-react/dist/commonjs/generic';
 
 interface SideNavMenuProps {
     horizontalDisplay?: boolean;
@@ -44,75 +45,29 @@ export const SideNavMenu: React.FunctionComponent<SideNavMenuProps> = ({
         },
     ];
 
-    if (horizontalDisplay) {
-        return (
-            <Menu icon='labeled' size='tiny' compact>
-                {navItems.map(item => (
-                    <Menu.Item
-                        key={item.path}
-                        active={activePath === item.path}
-                        onClick={(): void => history.push(item.path)}
-                    >
-                        <Icon className={item.iconName} />
-                        {item.name}
-                    </Menu.Item>
-                ))}
-            </Menu>
-        );
-    } else {
-        return (
-            <Menu vertical>
-                {navItems.map(item => (
-                    <Menu.Item
-                        key={item.path}
-                        active={activePath === item.path}
-                        onClick={(): void => history.push(item.path)}
-                    >
-                        <Icon className={item.iconName} />
-                        {item.name}
-                    </Menu.Item>
-                ))}
-            </Menu>
-        );
-    }
+    const horizontalDisplayProps: StrictMenuProps = {
+        icon: 'labeled',
+        size: 'tiny',
+        fluid: true,
+        widths: navItems.length as SemanticWIDTHS,
+    };
 
+    const verticalDisplayProps: StrictMenuProps = {
+        vertical: true,
+    };
+
+    return (
+        <Menu {...(horizontalDisplay ? horizontalDisplayProps : verticalDisplayProps)}>
+            {navItems.map(item => (
+                <Menu.Item
+                    key={item.path}
+                    active={activePath === item.path}
+                    onClick={(): void => history.push(item.path)}
+                >
+                    <Icon className={item.iconName} />
+                    {item.name}
+                </Menu.Item>
+            ))}
+        </Menu>
+    );
 };
-
-/*
-<Grid padded className="mobile only">
-                    <Menu borderless inverted fluid fixed="top">
-                        <Menu.Item header as="a">
-                        TipsMyWeb
-                        </Menu.Item>
-                        <Menu.Menu position="right">
-                            <Menu.Item>
-                                <Button
-                                    basic
-                                    inverted
-                                    icon
-                                    toggle
-                                    onClick={this.handleToggleDropdownMenu}
-                                >
-                                    <Icon name="content" />
-                                </Button>
-                            </Menu.Item>
-                        </Menu.Menu>
-                        <Menu
-                            borderless
-                            fluid
-                            inverted
-                            vertical
-                            style={this.state.dropdownMenuStyle}
-                        >
-                            {navItems.map(item => (
-                                <Link to={item.path} key={item.path}>
-                                    <Menu.Item>
-                                        {item.name}
-                                    </Menu.Item>
-                                </Link>
-                        ))}
-                            <Divider fitted />
-                        </Menu>
-                    </Menu>
-                </Grid>
- */
