cef945c1f6e5e3c4729c040bf7dd8b543a31a318
lancelotimb <lancelot.imb@gmail.com>
2020-02-24 03:31:08 +0100

    Add comment about strict mode

--- react/app-main/app-main-entry.tsx
+++ react/app-main/app-main-entry.tsx
@@ -3,6 +3,9 @@ import * as ReactDOM from 'react-dom';
 
 import { AppMain } from './AppMain';
 
+/*
+ * Enabling strict mode ensures we won't add deprecated/dangerous react patterns.
+ */
 ReactDOM.render(
     <React.StrictMode>
         <AppMain />
