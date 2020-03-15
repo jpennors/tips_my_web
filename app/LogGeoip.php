16dd9bcaee6ace8e0742b65ebc0de9b3abb6db85
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2020-03-15 12:41:59 +0100

    add attributes for loggeoip model

--- app/LogGeoip.php
+++ app/LogGeoip.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class LogGeoip extends Model
 {
-    //
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'log_geoips';
+
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+    protected $fillable = ['continent', 'timezone', 'country', 'state_name', 'city'];
+
 }
