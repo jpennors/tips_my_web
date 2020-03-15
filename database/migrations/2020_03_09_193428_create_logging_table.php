6715d5c5ab0d7e94be63ebceccf4c3b9127f6e4f
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2020-03-15 01:49:55 +0100

    edit file name

--- database/migrations/2020_03_09_193428_create_logging_table.php
+++ database/migrations/2020_03_09_193428_create_logging_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLogsTable extends Migration
+class CreateLoggingTable extends Migration
 {
     /**
      * Run the migrations.
@@ -19,7 +19,7 @@ class CreateLogsTable extends Migration
             $table->bigIncrements('id');
             $table->text('description')->nullable();
             $table->enum('level', ['emergency', 'alert', 'critical', 'error', 'warning', 'notice', 'info', 'debug']);
-            $table->string('hashed_ip');
+            $table->string('hashed_ip')->nullable();
             $table->string('created_date', 20);
             $table->index('created_date');
             $table->bigInteger('route_id')->nullable();
