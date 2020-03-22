35f698ca3124c0d243768a32fba5eabb603020e3
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2020-03-22 23:52:03 +0100

    Add methods to store, update and delete prices

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Price;
+use App\Http\Requests\PriceRequest;
+use App\Resource;
 
 class PriceController extends Controller
 {
@@ -16,5 +18,75 @@ class PriceController extends Controller
     {
         $prices = Price::all();
         return response()->json($prices, 200);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PriceRequest $request)
+    {
+
+        // Instance creation
+        $price = new Price();
+
+        // Try to save the price
+        try {
+            $prices = $price->create($request->all());
+        } catch(\Exception $e) {
+            abort(500, "Can't save the resource");
+        }
+
+        return response()->json(Price::find($prices->id), 201);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PriceRequest $request, $id)
+    {
+
+        $price = Price::findOrFail($id);
+
+        try {
+            $price->update($request->all());
+        } catch(\Exception $e) {
+            abort(500, "Can't update the price");
+        }
+
+        return response()->json($price, 200);
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $price = Price::findOrFail($id);
+
+        // Try to delete
+        try {
+            $resources = $price->resources();
+            foreach ($resources as $r) {
+                $r->price_id = null;
+                $r->save();
+            }
+            $price->delete();
+        } catch(\Exception $e) {
+            abort(500, "Can't delete the resource");
+        }
+        return response()->json();
     }
 }
