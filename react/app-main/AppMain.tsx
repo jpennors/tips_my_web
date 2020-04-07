75627d9bb2f9c6cf9408899e1cb7214614999dad
lancelotimb <lancelot.imb@gmail.com>
2020-04-07 15:41:17 +0200

    Add ViewportProvider to main app

--- react/app-main/AppMain.tsx
+++ react/app-main/AppMain.tsx
@@ -7,23 +7,26 @@ import { SearchPage } from 'tmw-main/components/SearchPage';
 import { SearchResultsPage } from 'tmw-main/components/SearchResultsPage';
 import { TermsPage } from 'tmw-main/components/TermsPage';
 import { MAIN_APP_ROUTES } from 'tmw-main/constants/app-constants';
+import { ViewportProvider } from 'tmw-common/components/ViewportProvider';
 
 import './app-main.css';
 
 export const AppMain: React.FunctionComponent = () => (
     <CookiesProvider>
-        <BrowserRouter>
-            <div className="app-main">
-                <LayoutHeader />
-                <Switch>
-                    <Route path={MAIN_APP_ROUTES.HOME} component={SearchPage} exact />
-                    <Route path={MAIN_APP_ROUTES.SEARCH} component={SearchPage} />
-                    <Route path={MAIN_APP_ROUTES.TERMS} component={TermsPage} />
-                    <Route path={MAIN_APP_ROUTES.RESULTS} component={SearchResultsPage} />
-                    <Redirect to={MAIN_APP_ROUTES.HOME} />
-                </Switch>
-                <LayoutFooter />
-            </div>
-        </BrowserRouter>
+        <ViewportProvider>
+            <BrowserRouter>
+                <div className="app-main">
+                    <LayoutHeader />
+                    <Switch>
+                        <Route path={MAIN_APP_ROUTES.HOME} component={SearchPage} exact />
+                        <Route path={MAIN_APP_ROUTES.SEARCH} component={SearchPage} />
+                        <Route path={MAIN_APP_ROUTES.TERMS} component={TermsPage} />
+                        <Route path={MAIN_APP_ROUTES.RESULTS} component={SearchResultsPage} />
+                        <Redirect to={MAIN_APP_ROUTES.HOME} />
+                    </Switch>
+                    <LayoutFooter />
+                </div>
+            </BrowserRouter>
+        </ViewportProvider>
     </CookiesProvider>
 );
