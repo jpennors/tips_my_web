ef8fcc4c3022749d8b70f8fc39b96a51066c4da4
lancelotimb <lancelot.imb@gmail.com>
2020-04-08 17:32:00 +0200

    Adjust bar height with screen size

--- react/app-main/components/TagsLaunchBar/TagsLaunchBar.tsx
+++ react/app-main/components/TagsLaunchBar/TagsLaunchBar.tsx
@@ -4,7 +4,7 @@ import { useViewport } from 'tmw-common/components/ViewportProvider';
 import './tags-launch-bar.css';
 
 const MAX_BAR_WIDTH = 600;
-const SEARCH_BUTTON_WIDTH = 45;
+const MAX_BAR_HEIGHT = 45;
 
 interface TagsLaunchBarProps {
     /* Bar width in % */
@@ -20,21 +20,22 @@ export const TagsLaunchBar: React.FunctionComponent<TagsLaunchBarProps> = ({
     const { width } = useViewport();
 
     const maxBarWidth = 0.88 * width < MAX_BAR_WIDTH ? 0.88 * width : MAX_BAR_WIDTH;
+    const barHeight = width < 450 ? width / 10 : MAX_BAR_HEIGHT;
 
     let barWidth = (completionPercentage * maxBarWidth) / 100;
     let isMinWidth = false;
 
     if (barWidth > maxBarWidth) {
         barWidth = maxBarWidth;
-    } else if (barWidth <= SEARCH_BUTTON_WIDTH) {
-        barWidth = SEARCH_BUTTON_WIDTH;
+    } else if (barWidth <= barHeight) {
+        barWidth = barHeight;
         isMinWidth = true;
     }
 
     return (
         <div
             className="tags-launch-bar"
-            style={{ width: `${barWidth}px`, height: `${SEARCH_BUTTON_WIDTH}px` }}
+            style={{ width: `${barWidth}px`, height: `${barHeight}px` }}
         >
             <a role="button" title="Launch Search">
                 {isMinWidth ? (
@@ -42,14 +43,14 @@ export const TagsLaunchBar: React.FunctionComponent<TagsLaunchBarProps> = ({
                         src="/images/chevron-down.svg"
                         alt="Launch Search"
                         className="tags-launch-bar__launch-chevron-icon"
-                        style={{ height: `${SEARCH_BUTTON_WIDTH}px` }}
+                        style={{ height: `${barHeight}px` }}
                     />
                 ) : (
                     <img
                         src="/images/tick.svg"
                         alt="Launch Search"
                         className="tags-launch-bar__launch-tick-icon"
-                        style={{ height: `${SEARCH_BUTTON_WIDTH}px` }}
+                        style={{ height: `${barHeight}px` }}
                         onClick={onClickCallback}
                     />
                 )}
