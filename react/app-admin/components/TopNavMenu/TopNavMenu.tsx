52e67b53bd36c31eea9d1e4a5bc712d07b6a4744
lancelotimb <lancelot.imb@gmail.com>
2020-04-10 13:57:04 +0200

    Replace logo

--- react/app-admin/components/TopNavMenu/TopNavMenu.tsx
+++ react/app-admin/components/TopNavMenu/TopNavMenu.tsx
@@ -8,7 +8,7 @@ export const TopNavMenu: React.FunctionComponent = () => {
         <Menu inverted style={{ borderRadius: 0 }}>
             <Container>
                 <Menu.Item href={ADMIN_APP_ROUTES.MAIN} header>
-                    <Image size="mini" src="/images/logo.png" style={{ marginRight: '5px' }} />
+                    <Image size="mini" src="/images/logo-icon.svg" style={{ marginRight: '5px' }} />
                     TipsMyWeb Admin
                 </Menu.Item>
                 <Menu.Item onClick={logout}>Logout</Menu.Item>
