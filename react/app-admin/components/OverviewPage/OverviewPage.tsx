606f139bfec0fd4882df1f7f8fb19251596a960b
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2020-04-10 17:36:53 +0200

    wip add chart to overview tab

--- react/app-admin/components/OverviewPage/OverviewPage.tsx
+++ react/app-admin/components/OverviewPage/OverviewPage.tsx
@@ -5,6 +5,7 @@ import { PageHeader } from 'tmw-admin/components/PageHeader';
 import { serializeLogsFromAPI } from 'tmw-admin/utils/api-serialize';
 import { ajaxGet, ajaxPost } from 'tmw-common/utils/ajax';
 import { Log } from 'tmw-admin/constants/app-types';
+import {Line} from 'react-chartjs-2';
 
 export const OverviewPage: React.FunctionComponent = () => {
     const [visitorsNumber, setVisitorNumbers] = React.useState<number>(0);
@@ -12,8 +13,38 @@ export const OverviewPage: React.FunctionComponent = () => {
     const [isLoading, setIsLoading] = React.useState<boolean>(true);
     const [errorMessage, setErrorMessage] = React.useState<string>('');
     const hasError = errorMessage.length > 0;
+    var data = {
+        labels: [""],
+        datasets: [
+          {
+            label: 'Visitors',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0]
+          }
+        ]
+      };
 
     const fetchVisitorNumbers = async (): Promise<void> => {
+        const endDate = new Date();
+        const startDate = new Date(endDate.setMonth(endDate.getMonth()-1));
+
+        console.log(startDate);
         return ajaxGet('current/visitors')
             .then(res => {
                 setVisitorNumbers(res.data.visitors);
@@ -22,6 +53,20 @@ export const OverviewPage: React.FunctionComponent = () => {
                 setErrorMessage('Error while fetching visitors number from API.');
             });
     };
+
+    const fetchVisitors = async() : Promise<void> => {
+        return ajaxGet('visitors')
+            .then(res => {
+                for (let index = 0; index < res.data.length; index++) {
+                    data.labels.push(res.data[index].created_date);
+                    // data.datasets.data.push(int(res.data[index].visitors));
+                }
+                console.log(res.data);
+            })
+            .catch(() => {
+                setErrorMessage('Error while fetching visitors number from API.');
+            });
+    }
 
     const fetchLogs = async (): Promise<void> => {
         return ajaxPost('logs', { date: '2020-03-15' })
@@ -38,6 +83,7 @@ export const OverviewPage: React.FunctionComponent = () => {
         fetchVisitorNumbers().finally(() => {
             setIsLoading(false);
         });
+        fetchVisitors();
         fetchLogs();
     }, []);
 
@@ -72,6 +118,7 @@ export const OverviewPage: React.FunctionComponent = () => {
                     <p>
                         Number of visitors today : <strong>{visitorsNumber}</strong>
                     </p>
+                    <Line data={data} />
                 </Message>
             )}
 
