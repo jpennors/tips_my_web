ef98bd2267c5f61a7c4eb1939301c1813d97682e
lancelotimb <lancelot.imb@gmail.com>
2020-04-10 12:32:32 +0200

    Use enum for breakpoint values

--- react/app-main/components/LayoutHeader/LayoutHeader.tsx
+++ react/app-main/components/LayoutHeader/LayoutHeader.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { ContactModalContent } from 'tmw-main/components/ContactModalContent';
 import { HeaderModal } from 'tmw-main/components/HeaderModal';
 import { SuggestionModalContent } from 'tmw-main/components/SuggestionModalContent';
-import { MAIN_APP_ROUTES } from 'tmw-main/constants/app-constants';
+import { MAIN_APP_ROUTES, VIEWPORT_BREAKPOINTS } from 'tmw-main/constants/app-constants';
 import { useViewport } from 'tmw-common/components/ViewportProvider';
 import { MagnifyingGlassIcon } from 'tmw-main/icons/MagnifyingGlassIcon';
 
@@ -12,7 +12,7 @@ import './layout-header.css';
 
 export const LayoutHeader: React.FunctionComponent = () => {
     const { width } = useViewport();
-    const isMobileViewport = width < 450;
+    const isMobileViewport = width < VIEWPORT_BREAKPOINTS.MOBILE;
 
     const links = [
         {
