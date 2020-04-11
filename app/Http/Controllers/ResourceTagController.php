fe5d65d0ace9cb23f66beb779d32a47c020ce7f8
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2020-04-11 23:26:36 +0200

    WIP Work on a new algorithm to search resources based on requested tags

--- app/Http/Controllers/ResourceTagController.php
+++ app/Http/Controllers/ResourceTagController.php
@@ -17,37 +17,17 @@ class ResourceTagController extends Controller
      */
     public function search(Request $request)
     {
+        // Retrieve tags id of requested tags
         $search_tags_slugs = $request->tags;
-        $search_size = sizeof($search_tags_slugs);
-        $search_results = array();
+        $tags_id = Tag::whereIn('slug', $search_tags_slugs)
+            ->pluck('id')
+            ->toArray();
+        
+        // Retrieve all resources 
+        $resource_tags = ResourceTag::with('resource')->whereIn('tag_id', $tags_id)->get();
 
-        foreach ($search_tags_slugs as $tag_slug) {
-            $tag = Tag::where('slug', $tag_slug)->firstOrFail();
-            $resource_tags = ResourceTag::with('resource')->where('tag_id', $tag["id"])->get();
-            foreach ($resource_tags as $resource_tag) {
-
-                $index = array_search($resource_tag->resource['id'], array_column($search_results, 'id'));
-
-                // If resource not found in the search_results, add it with a search_score
-                // If not, sum the search_score
-                if($index === FALSE){
-                    // Get resource with price
-                    $resource = Resource::where('id', $resource_tag->resource['id'])->with('price')->get()[0];
-
-                    $resource["search_score"] = $resource_tag->belonging/$search_size;
-                    array_push($search_results, $resource);
-                } else {
-                    $search_results[$index]["search_score"] += $resource_tag->belonging/$search_size;
-                }
-            }
-        }
-
-        // Sort the search_results by search_score value (descending)
-        usort($search_results,function($first,$second){
-            return $first["search_score"] < $second["search_score"];
-        });
-
-        return response()->json($search_results);
+        ResourceTag::getRecommendedResources($resource_tags);
+                
     }
 
 }
