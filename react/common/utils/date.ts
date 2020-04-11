4830b8c636ea0af96f4a100ad0c89589cfae4932
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2020-04-11 18:35:38 +0200

    Create date util

---
+++ react/common/utils/date.ts
@@ -0,0 +1,9 @@
+
+export function getApiDateFormat(date: Date) {
+
+    const day = ("0" + (date.getDate())).slice(-2);
+    const month_number = ("0" + (date.getMonth() + 1)).slice(-2)
+    const year = date.getFullYear();
+
+    return year + "-" + month_number + "-" + day;
+}
