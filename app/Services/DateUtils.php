803f5d7a19206af978f51d334e5fb176f8aa36f0
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2020-04-11 16:08:32 +0200

    Create DateUtils service, add to function to get date range

---
+++ app/Services/DateUtils.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Carbon;
+
+class DateUtils
+{
+
+
+    public static function getDateRange($start_date, $end_date){
+        
+        $start_date = Carbon::createFromFormat('Y-m-d', $start_date);
+        $end_date = Carbon::createFromFormat('Y-m-d', $end_date);
+        $dates = array();
+
+        for($date = $start_date; $date->lte($end_date); $date->addDay()) {
+
+            array_push($dates, 
+                array($date->format('Y-m-d') => array(
+                    "formatted_date" => $date->format('m-d'),
+                    "visitors"  => 0
+                ))
+            );
+            $dates[] = $date->format('Y-m-d');
+    
+        }
+        
+        return $dates;
+    }
+}
