ef5e32e6b64eb2f1ad493bb3bd94ca1d2ffb937c
Josselin Pennors <josselin.pennors@hotmail.fr>
2020-04-19 01:33:11 +0200

    Add try catch in deployment methods

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -10,34 +10,54 @@ class DeploymentController extends Controller
 
     public function DependenciesInstallationCommand(Request $request)
     {
-        $command = "php composer.phar install"; 
-        $path = base_path();
-        exec("cd {$path} && {$command}");
+        try {
+            $command = "php composer.phar install"; 
+            $path = base_path();
+            exec("cd {$path} && {$command}");
+        } catch(\Exception $e) {
+            abort(500, $e);
+        }
         return response()->json();
     }
 
 
     public function DatabaseMigrationCommand(Request $request)
     {
-        Artisan::call('migrate');
+        try {
+            Artisan::call('migrate');
+        } catch(\Exception $e) {
+            abort(500, $e);
+        }
         return response()->json();
     }
 
     public function DatabaseSeedingCommand(Request $request)
     {
-        Artisan::call('db:seed');
+        try {
+            Artisan::call('db:seed');
+        } catch(\Exception $e) {
+            abort(500, $e);
+        }
         return response()->json();
     }
 
     public function CacheClearCommand(Request $request)
     {
-        Artisan::call('cache:clear');
+        try {
+            Artisan::call('cache:clear');
+        } catch(\Exception $e) {
+            abort(500, $e);
+        }
         return response()->json();
     }
 
     public function ConfigClearCommand(Request $request)
     {
-        Artisan::call('config:clear');
+        try {
+            Artisan::call('config:clear');
+        } catch(\Exception $e) {
+            abort(500, $e);
+        }
         return response()->json();
     }
 
