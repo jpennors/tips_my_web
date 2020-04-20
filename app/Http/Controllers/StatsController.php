9cf61e1decece74f7c1ae6317b3d1205abc428b4
Josselin Pennors <josselin.pennors@hotmail.fr>
2020-04-20 22:30:45 +0200

    Implement function to get all time best resources

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Resource;
-use Carbon\Carbon;
+use App\Services\DateUtils;
 use DB;
 use App\StatTag;
+use App\StatResource;
 
 class StatsController extends Controller
 {
@@ -23,7 +24,6 @@ class StatsController extends Controller
     }
 
 
-    public function getTopResources()
     public function GetTopTrendyResources(Request $request)
     {
         // Date formate Y-m-d
@@ -39,14 +39,18 @@ class StatsController extends Controller
         return response()->json($trendy_resources, 200);
     }
 
+
+    public function GetTopAllTimeResources()
+    {
+        $quantity = 10;
         $top_resources_all_time = array();
         
         $top_resources_all_time["visits"] = Resource::orderBy('visits', 'DESC')
-            ->take(10)
+            ->take($quantity)
             ->get();
     
         $top_resources_all_time["like"] = Resource::orderBy('like', 'DESC')
-            ->take(10)
+            ->take($quantity)
             ->get();
 
         return response()->json($top_resources_all_time, 200);
