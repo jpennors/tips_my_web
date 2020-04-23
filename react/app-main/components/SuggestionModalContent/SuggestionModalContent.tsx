409bd25d95bf3ef32e6e2dc9bb5b086ded84e6ee
lancelotimb <lancelot.imb@gmail.com>
2020-04-24 01:17:21 +0200

    Add validation on suggestions modal

--- react/app-main/components/SuggestionModalContent/SuggestionModalContent.tsx
+++ react/app-main/components/SuggestionModalContent/SuggestionModalContent.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-
-import { BigButton } from 'tmw-main/components/BigButton';
+import { SubmitButton } from 'tmw-main/components/SubmitButton';
+import { isValidUrl } from 'tmw-main/utils/form-validation';
 import { ajaxPost } from 'tmw-common/utils/ajax';
 import { InputField } from 'tmw-main/components/InputField';
 
@@ -9,39 +9,78 @@ import './suggestion-modal-content.less';
 export const SuggestionModalContent: React.FunctionComponent = () => {
     const [addressInputValue, setAddressInputValue] = React.useState<string>('');
     const [descriptionInputValue, setDescriptionInputValue] = React.useState<string>('');
+    const [addressValidationMessage, setAddressValidationMessage] = React.useState<string>('');
+    const [descriptionValidationMessage, setDescriptionValidationMessage] = React.useState<string>(
+        '',
+    );
+
+    const [hasSubmitError, setHasSubmitError] = React.useState<boolean>(false);
+    const [hasSubmitSuccess, setHasSubmitSuccess] = React.useState<boolean>(false);
+    const [isSubmitPending, setIsSubmitPending] = React.useState<boolean>(false);
 
     const handleAddressInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         const { value } = event.target;
-        // TODO: Add validation
         setAddressInputValue(value);
     };
 
     const handleDescriptionInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
         const { value } = event.target;
-        // TODO: Add validation
         setDescriptionInputValue(value);
     };
 
     const submitSuggestionForm = async (): Promise<void> => {
-        // TODO: Add validation
         const payload = {
             url: addressInputValue,
             description: descriptionInputValue,
         };
 
-        ajaxPost('suggestions', payload).then(() => {
-            setAddressInputValue('');
-            setDescriptionInputValue('');
-        });
+        let isFormValid = true;
 
-        // TODO: Handle ajax errors
+        // URL validation
+        if (!addressInputValue) {
+            setAddressValidationMessage("You need to provide the website's url here");
+            isFormValid = false;
+        } else if (!isValidUrl(addressInputValue)) {
+            setAddressValidationMessage('The provided URL is not valid');
+            isFormValid = false;
+        } else {
+            setAddressValidationMessage('');
+        }
+
+        // Description validation
+        if (!descriptionInputValue) {
+            setDescriptionValidationMessage('Please add a quick description');
+            isFormValid = false;
+        } else {
+            setDescriptionValidationMessage('');
+        }
+
+        if (isFormValid) {
+            setIsSubmitPending(true);
+            ajaxPost('suggestions', payload)
+                .then(() => {
+                    setHasSubmitSuccess(true);
+                })
+                .catch(error => {
+                    // Additional error messages from backend validation (shouldn't happen)
+                    const errorMessages = error.response?.data?.errors;
+                    setAddressValidationMessage(errorMessages?.url || '');
+                    setDescriptionValidationMessage(errorMessages?.description || '');
+                    setHasSubmitError(true);
+                })
+                .finally(() => {
+                    setIsSubmitPending(false);
+                });
+        }
     };
 
     return (
         <div className="suggestion-modal-content">
             <div className="suggestion-modal-content__title">Share a website</div>
             <div className="suggestion-modal-content__subtitle">
-                Share your favorite resources with the community
+                {!hasSubmitSuccess
+                    ? 'Share your favorite resources with the community!'
+                    : 'Your suggestion was successfully submitted!'}
             </div>
             <div className="suggestion-modal-content__body">
                 <form className="suggestion-modal-content__form">
@@ -53,6 +92,9 @@ export const SuggestionModalContent: React.FunctionComponent = () => {
                         value={addressInputValue}
                         isRequired
                         onChange={handleAddressInputChange}
+                        validationMessage={addressValidationMessage}
+                        isInvalid={addressValidationMessage.length > 0}
+                        isDisabled={isSubmitPending || hasSubmitSuccess}
                     />
                     <InputField
                         type="textarea"
@@ -62,12 +104,24 @@ export const SuggestionModalContent: React.FunctionComponent = () => {
                         value={descriptionInputValue}
                         isRequired
                         onChange={handleDescriptionInputChange}
+                        validationMessage={descriptionValidationMessage}
+                        isInvalid={descriptionValidationMessage.length > 0}
+                        isDisabled={isSubmitPending || hasSubmitSuccess}
                     />
                 </form>
             </div>
             <div className="suggestion-modal-content__buttons">
-                <BigButton content="SUBMIT" onClick={submitSuggestionForm} />
+                <SubmitButton
+                    onClick={submitSuggestionForm}
+                    isValid={hasSubmitSuccess}
+                    isPending={isSubmitPending}
+                />
             </div>
+            {hasSubmitError ? (
+                <div className="contact-modal-content__submit-error">
+                    We&apos;re having trouble submitting your message, please try again!
+                </div>
+            ) : null}
         </div>
     );
 };
--- react/app-main/utils/form-validation.ts
+++ react/app-main/utils/form-validation.ts
@@ -2,3 +2,10 @@ export const isValidEmail = (email: string): boolean => {
     const emailRegex = RegExp(/^[^@]+@[^@]+\.[^@]+$/);
     return emailRegex.test(email);
 };
+
+export const isValidUrl = (url: string): boolean => {
+    const emailRegex = RegExp(
+        /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=]+$/,
+    );
+    return emailRegex.test(url);
+};
