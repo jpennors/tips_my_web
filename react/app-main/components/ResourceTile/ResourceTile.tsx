4621184c0125bb2d9ed6d208e71ec49ff471e2c4
lancelotimb <lancelot.imb@gmail.com>
2020-04-26 22:03:59 +0200

    Update "Open website" button position

--- react/app-main/components/ResourceTile/ResourceTile.tsx
+++ react/app-main/components/ResourceTile/ResourceTile.tsx
@@ -42,47 +42,47 @@ export const ResourceTile: React.FunctionComponent<ResourceTileProps> = ({ resou
 
     return (
         <div className="resource-tile">
-            {!isMobileViewport ? (
-                <div className="resource-tile__header">
-                    <span className="resource-tile__header-dot resource-tile__header-dot--red" />
-                    <span className="resource-tile__header-dot resource-tile__header-dot--yellow" />
-                    <span className="resource-tile__header-dot resource-tile__header-dot--green" />
-                </div>
-            ) : null}
-            <a
-                href={resource.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={(): Promise<void> => visitWebsite(resource.id)}
-            >
-                <img src={iconUrl} alt={resource.name} className="resource-tile__icon" />
-            </a>
-            <div className="resource-tile__content">
-                <div className="resource-tile__title-float-right">
-                    <ResourcePricingPill pricing={resource.pricing} />
-                    <span className="resource-tile__like-resource-button">
-                        <img
-                            src={isLiked ? '/images/heart-full.svg' : '/images/heart.svg'}
-                            alt={isLiked ? 'Unlike' : 'Like'}
-                            height="15px"
-                            onClick={likeResource}
-                        />
-                    </span>
-                </div>
-                <p className="resource-tile__title">{resource.name}</p>
-                <p className="resource-tile__description">{resource.description}</p>
-            </div>
-            <div className="resource-tile__actions">
+            <div className="resource-tile__container">
+                {!isMobileViewport ? (
+                    <div className="resource-tile__header">
+                        <span className="resource-tile__header-dot resource-tile__header-dot--red" />
+                        <span className="resource-tile__header-dot resource-tile__header-dot--yellow" />
+                        <span className="resource-tile__header-dot resource-tile__header-dot--green" />
+                    </div>
+                ) : null}
                 <a
                     href={resource.url}
-                    onClick={(): Promise<void> => visitWebsite(resource.id)}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="resource-tile__visit-resource-button"
+                    onClick={(): Promise<void> => visitWebsite(resource.id)}
                 >
-                    Visit →
+                    <img src={iconUrl} alt={resource.name} className="resource-tile__icon" />
                 </a>
+                <div className="resource-tile__content">
+                    <div className="resource-tile__title-float-right">
+                        <ResourcePricingPill pricing={resource.pricing} />
+                        <span className="resource-tile__like-resource-button">
+                            <img
+                                src={isLiked ? '/images/heart-full.svg' : '/images/heart.svg'}
+                                alt={isLiked ? 'Unlike' : 'Like'}
+                                height="15px"
+                                onClick={likeResource}
+                            />
+                        </span>
+                    </div>
+                    <p className="resource-tile__title">{resource.name}</p>
+                    <p className="resource-tile__description">{resource.description}</p>
+                </div>
             </div>
+            <a
+                href={resource.url}
+                onClick={(): Promise<void> => visitWebsite(resource.id)}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="resource-tile__visit-resource-button"
+            >
+                Open website
+            </a>
         </div>
     );
 };
