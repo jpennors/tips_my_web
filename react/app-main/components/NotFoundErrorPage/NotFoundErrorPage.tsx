d867f1c586a7ca414ec2cc7a8ceade39b65f0e3f
lancelotimb <lancelot.imb@gmail.com>
2020-04-27 23:30:59 +0200

    Add "Back home" button

--- react/app-main/components/NotFoundErrorPage/NotFoundErrorPage.tsx
+++ react/app-main/components/NotFoundErrorPage/NotFoundErrorPage.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { BigButton } from 'tmw-main/components/BigButton';
+import { MAIN_APP_ROUTES } from 'tmw-main/constants/app-constants';
 
 import './not-found-error-page.less';
 
@@ -12,6 +15,11 @@ export const NotFoundErrorPage: React.FunctionComponent = () => {
                     className="not-found-error-page__image"
                 />
                 <div className="not-found-error-page__text">Page not found</div>
+            </div>
+            <div className="not-found-error-page__back-button">
+                <Link to={MAIN_APP_ROUTES.HOME}>
+                    <BigButton content="Back to Home Page" isColored={false} />
+                </Link>
             </div>
         </div>
     );
