57ff155c453d15a4e20506edbe55cadc11a07e0e
Josselin Pennors <josselin.pennors@hotmail.fr>
2020-05-02 23:32:36 +0200

    Add Resource Tag seeder

--- database/seeds/ResourceTagTablesSeeder.php
+++ database/seeds/ResourceTagTablesSeeder.php
@@ -19,6 +19,128 @@ class ResourceTagTablesSeeder extends Seeder
         
         if (config('app.env') === 'development') {
             
+            $resources = [
+                array(
+                    'name'  => 'Vexel',
+                    'description'  => 'Download Royalty-free Commercial Use Vector Graphics and Images. 
+                        Trendy vectors and more. Commercial License Vector. Download Vector, PNG, SVG. 
+                        Royalty Free Vectors.',
+                    'url'   =>  'https://www.vexels.com/',
+                    'language'  =>  'fr',
+                    'score'     =>  10,
+                    'price_id'  => Price::first()->id,
+                    'type_id'   => Type::first()->id,
+                    'tags'  => [
+                        'communication',
+                        'graphisme',
+                        'web'
+                    ],
+                ),
+                array(
+                    'name'  => 'unDraw',
+                    'description'  => 'Download Royalty-free Commercial Use Vector Graphics and Images. 
+                        Trendy vectors and more. Commercial License Vector. Download Vector, PNG, SVG. 
+                        Royalty Free Vectors.',
+                    'url'   =>  'https://www.vexels.com/',
+                    'language'  =>  'fr',
+                    'score'     =>  10,
+                    'price_id'  => Price::first()->id,
+                    'type_id'   => Type::first()->id,
+                    'tags'  => [
+                        'ui',
+                        'graphisme',
+                    ],
+                ),
+                array(
+                    'name'  => 'Pexel',
+                    'description'  => 'Download Royalty-free Commercial Use Vector Graphics and Images. 
+                        Trendy vectors and more. Commercial License Vector. Download Vector, PNG, SVG. 
+                        Royalty Free Vectors.',
+                    'url'   =>  'https://www.vexels.com/',
+                    'language'  =>  'fr',
+                    'score'     =>  10,
+                    'price_id'  => Price::first()->id,
+                    'type_id'   => Type::first()->id,
+                    'tags'  => [
+                        'ux',
+                        'graphisme',
+                    ],
+                ),
+                array(
+                    'name'  => 'Canva',
+                    'description'  => 'Download Royalty-free Commercial Use Vector Graphics and Images. 
+                        Trendy vectors and more. Commercial License Vector. Download Vector, PNG, SVG. 
+                        Royalty Free Vectors.',
+                    'url'   =>  'https://www.vexels.com/',
+                    'language'  =>  'fr',
+                    'score'     =>  10,
+                    'price_id'  => Price::first()->id,
+                    'type_id'   => Type::first()->id,
+                    'tags'  => [
+                        'web',
+                        'canva',
+                        'graphisme',
+                    ],
+                ),
+                array(
+                    'name'  => 'Unsplash',
+                    'description'  => 'Download Royalty-free Commercial Use Vector Graphics and Images. 
+                        Trendy vectors and more. Commercial License Vector. Download Vector, PNG, SVG. 
+                        Royalty Free Vectors.',
+                    'url'   =>  'https://www.vexels.com/',
+                    'language'  =>  'fr',
+                    'score'     =>  10,
+                    'price_id'  => Price::first()->id,
+                    'type_id'   => Type::first()->id,
+                    'tags'  => [
+                        'graphisme',
+                        'photo'
+                    ],
+                ),
+                array(
+                    'name'  => 'Coolors',
+                    'description'  => 'Download Royalty-free Commercial Use Vector Graphics and Images. 
+                        Trendy vectors and more. Commercial License Vector. Download Vector, PNG, SVG. 
+                        Royalty Free Vectors.',
+                    'url'   =>  'https://www.vexels.com/',
+                    'language'  =>  'fr',
+                    'score'     =>  10,
+                    'price_id'  => Price::first()->id,
+                    'type_id'   => Type::first()->id,
+                    'tags'  => [
+                        'web',
+                        'graphisme',
+                        'color'
+                    ],
+                ),
+                        
+            ];
+
+            foreach ($resources as $resource) {
+                $tags = $resource['tags'];
+                unset($resource['tags']);
+                $r = Resource::create($resource);
+                foreach($tags as $tag_name){
+                    $tag = Tag::firstOrCreate(['name' => $tag_name]);
+                    $rt = new ResourceTag();
+                    $rt->tag_id = $tag['id'];
+                    $rt->resource_id = $r->id;
+                    $rt->save();
+                }
+            }
+
+            // To do 
+            // After Tag rebase, add primary attribute to tags here
+
+            $primary_tags = [
+
+            ];
+
+            foreach($primary_tags as $tag_name){
+                $tag = Tag::firstOrCreate(['name' => $tag_name]);
+                // Primary attribute to add
+                $tag->save();
+            }
 
         }
 
