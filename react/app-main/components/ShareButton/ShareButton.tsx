fcc7d9faa808933d65e2b76df69e3076ae8b2c43
lancelotimb <lancelot.imb@gmail.com>
2020-05-11 00:18:46 +0200

    Add ToastMessage confirmation

--- react/app-main/components/ShareButton/ShareButton.tsx
+++ react/app-main/components/ShareButton/ShareButton.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
-import { BigButton } from 'tmw-main/components/BigButton';
 import * as copyTextToClipboard from 'copy-text-to-clipboard';
+import { BigButton } from 'tmw-main/components/BigButton';
+import { useToastMessageContext } from 'tmw-main/components/ToastMessage';
 
 import './share-button.less';
 
 export const ShareButton: React.FunctionComponent = () => {
+    const { openToastMessage } = useToastMessageContext();
+
     const onButtonClick = (): void => {
         copyTextToClipboard(window.location.href);
+        openToastMessage('URL was successfully copied in clipboard !');
     };
 
     return <BigButton content="Share" isColored={false} onClick={onButtonClick} />;
