c26e39b06bd3c878e58acd271f3ce2fba2309672
Josselin Pennors <josselin.pennors@hotmail.fr>
2020-08-24 11:06:43 +0200

    Edit database config for Heroku production

--- config/database.php
+++ config/database.php
@@ -1,5 +1,19 @@
 <?php
 
+$HOST = env('DB_HOST', '127.0.0.1');
+$USERNAME = env('DB_USERNAME', 'forge');
+$PASSWORD = env('DB_PASSWORD', '');
+$DATABASE = env('DB_DATABASE', 'forge');
+
+if (config('app.env') === 'production') {
+    $DATABASE_URL = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $HOST = $url["host"];
+    $USERNAME = $url["user"];
+    $PASSWORD = $url["pass"];
+    $DATABASE = substr($url["path"], 1);
+}
+
+
 return [
 
     /*
@@ -42,11 +56,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $HOST,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $DATABASE,
+            'username' => $USERNAME,
+            'password' => $PASSWORD,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
