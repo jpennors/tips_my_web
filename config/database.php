e93b94390528070496a7b1dd46da0d477c1c88cf
Josselin Pennors <josselin.pennors@hotmail.fr>
2020-08-24 11:15:00 +0200

    Remove database config

--- config/database.php
+++ config/database.php
@@ -1,19 +1,5 @@
 <?php
 
-$HOST = env('DB_HOST', '127.0.0.1');
-$USERNAME = env('DB_USERNAME', 'forge');
-$PASSWORD = env('DB_PASSWORD', '');
-$DATABASE = env('DB_DATABASE', 'forge');
-
-if (config('app.env') === 'production') {
-    $DATABASE_URL = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $HOST = $url["host"];
-    $USERNAME = $url["user"];
-    $PASSWORD = $url["pass"];
-    $DATABASE = substr($url["path"], 1);
-}
-
-
 return [
 
     /*
@@ -56,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $HOST,
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $DATABASE,
-            'username' => $USERNAME,
-            'password' => $PASSWORD,
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
