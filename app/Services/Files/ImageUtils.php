3ecc235023203f57ff0aa04875cb4ff44681b50c
Josselin Pennors <josselin.pennors@hotmail.fr>
2020-08-25 00:35:57 +0200

    Create Image Utils to get extension from file & url

---
+++ app/Services/Files/ImageUtils.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services\Files;
+
+use \Gumlet\ImageResize;
+
+class ImageUtils
+{
+
+    /**
+     * Search for image extension from url
+     * 
+     */
+    public static function getImageExtensionFromUrl($url)
+    {
+        try{
+
+            $headers = get_headers($url);
+        
+            foreach ($headers as $header) {
+                if (strpos($header, 'Content-Type') !== false) {
+                    $pos = strpos($header, 'image');
+                    if ($pos !== false) {
+                        $extension = substr($header, $pos + strlen('image') + 1);
+                        $pos = strpos($extension, ';');
+                        if ($pos !== false) {
+                            $extension = substr($extension, 0, $pos - 1);
+                        }
+            
+                        return $extension;
+                    }
+                }
+            }
+        } catch (\Throwable $th){}
+
+        return null;
+    }
+
+
+    /**
+     * Search for image extension from file
+     * 
+     */
+    public static function getImageExtensionFromFile($file)
+    {
+        return $file->guessExtension();
+    }
+
+}
