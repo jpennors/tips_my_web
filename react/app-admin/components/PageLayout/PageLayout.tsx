d05cac73daa76e3b7bcff1d2e42be0a2850df7ac
Josselin Pennors <josselin.pennors@hotmail.fr>
2020-09-01 16:45:45 +0200

    Link prices routes to components

--- react/app-admin/components/PageLayout/PageLayout.tsx
+++ react/app-admin/components/PageLayout/PageLayout.tsx
@@ -7,6 +7,8 @@ import { OverviewPage } from 'tmw-admin/components/OverviewPage';
 import { ContactPage } from 'tmw-admin/components/ContactPage';
 import { ResourcesEditPage } from 'tmw-admin/components/ResourcesEditPage';
 import { ResourcesPage } from 'tmw-admin/components/ResourcesPage';
+import { PricesPage } from 'tmw-admin/components/PricesPage';
+import { PricesEditPage } from 'tmw-admin/components/PricesEditPage';
 import { SideNavMenu } from 'tmw-admin/components/SideNavMenu';
 import { SuggestionsPage } from 'tmw-admin/components/SuggestionsPage';
 import { TagsEditPage } from 'tmw-admin/components/TagsEditPage';
@@ -85,6 +87,21 @@ export const PageLayout: React.FunctionComponent = () => {
                                         path={ADMIN_APP_ROUTES.RESOURCES_EDIT}
                                         exact
                                         component={ResourcesEditPage}
+                                    />
+                                    <ProtectedRoute
+                                        path={ADMIN_APP_ROUTES.PRICES}
+                                        exact
+                                        component={PricesPage}
+                                    />
+                                    <ProtectedRoute
+                                        path={ADMIN_APP_ROUTES.PRICES_ADD}
+                                        exact
+                                        component={PricesEditPage}
+                                    />
+                                    <ProtectedRoute
+                                        path={ADMIN_APP_ROUTES.PRICES_EDIT}
+                                        exact
+                                        component={PricesEditPage}
                                     />
                                     <ProtectedRoute
                                         path={ADMIN_APP_ROUTES.STATISTICS}
