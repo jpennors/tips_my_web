e86299e6549263af6719b4744041f69e026625c6
Josselin Pennors <josselin.pennors@hotmail.fr>
2020-09-01 16:45:31 +0200

    WIP Add Price Edit page

---
+++ react/app-admin/components/PricesEditPage/PricesEditPage.tsx
@@ -0,0 +1,139 @@
+import * as React from 'react';
+import { useParams } from 'react-router';
+import { Form, Message, StrictDropdownItemProps } from 'semantic-ui-react';
+import { ActionMessage } from 'tmw-admin/components/ActionMessage';
+import { FormFooter } from 'tmw-admin/components/FormFooter';
+import { PageHeader } from 'tmw-admin/components/PageHeader';
+import { ADMIN_APP_ROUTES } from 'tmw-admin/constants/app-constants';
+import { Price } from 'tmw-admin/constants/app-types';
+import { serializePricesFromAPI, serializePriceToAPI } from 'tmw-admin/utils/api-serialize';
+import { convertToSelectOptions } from 'tmw-admin/utils/select-options';
+import { ajaxGet, ajaxPost, ajaxPut } from 'tmw-common/utils/ajax';
+
+export const PricesEditPage: React.FunctionComponent = () => {
+    const [price, setPrice] = React.useState<Partial<Price>>({});
+    const isReadyToSubmit = price.name && price.name.length > 0;
+
+    const [isLoading, setIsLoading] = React.useState<boolean>(true);
+    const [canEdit, setCanEdit] = React.useState<boolean>(true);
+    const [errorMessage, setErrorMessage] = React.useState<string>('');
+    const [successMessage, setSuccessMessage] = React.useState<string>('');
+
+    const { id: editedPriceId } = useParams();
+
+    const fetchPriceOptions = async (): Promise<void> => {
+        return ajaxGet('prices')
+            .then(res => {
+                const prices = serializePricesFromAPI(res.data);
+
+                if (editedPriceId) {
+                    const editedPrice = prices.find(price => price.id === editedPriceId);
+                    if (editedPrice) {
+                        setPrice(editedPrice);
+                    } else {
+                        setErrorMessage('No matching price was found for this ID.');
+                        setCanEdit(false);
+                    }
+                }
+            })
+            .catch(() => {
+                setErrorMessage('Error while trying to fetch price data from the API.');
+                setCanEdit(false);
+            });
+    };
+
+    const onPriceNameInputChange = (_: any, { value }: { value: string }): void => {
+        setPrice({ ...price, name: value });
+    };
+
+    const resetForm = (): void => {
+        setPrice({});
+    };
+
+    const savePrice = (): void => {
+        setSuccessMessage('');
+        setErrorMessage('');
+        setIsLoading(true);
+        const newPrice = serializePriceToAPI(price);
+        newPrice.id = editedPriceId;
+
+        if (editedPriceId) {
+            ajaxPut(`prices/${editedPriceId}`, newPrice)
+                .then(() => {
+                    setSuccessMessage('Your price "' + price.name + '" was successfully edited.');
+                })
+                .catch(() => {
+                    setErrorMessage(
+                        'Error while updating the price. Your modifications were probably not saved.',
+                    );
+                })
+                .finally(() => {
+                    setIsLoading(false);
+                });
+        } else {
+            ajaxPost('prices', newPrice)
+                .then(() => {
+                    setSuccessMessage('Your new price "' + price.name + '" was successfully saved.');
+                    resetForm();
+                })
+                .catch(() => {
+                    setErrorMessage(
+                        'Error while posting new price to API. The new price was probably not saved.',
+                    );
+                })
+                .finally(() => {
+                    setIsLoading(false);
+                });
+        }
+    };
+
+    React.useEffect(() => {
+        fetchPriceOptions().finally(() => {
+            setIsLoading(false);
+        });
+    }, []);
+
+    return (
+        <div>
+            <PageHeader
+                iconName="tags"
+                headerContent={editedPriceId ? 'Edit Price' : 'Add Price'}
+                subHeaderContent={
+                    editedPriceId
+                        ? 'Edit an existing price'
+                        : 'Add a price to be used to search for resources'
+                }
+            />
+            <ActionMessage type="success" message={successMessage} />
+            <ActionMessage type="error" message={errorMessage} />
+            {canEdit && (
+                <>
+                    <Message
+                        attached
+                        header={
+                            editedPriceId ? 'Edit an existing price' : 'Add a price to filter resources'
+                        }
+                        content="A price can also be attached to a parent price"
+                    />
+                    <Form className="attached fluid segment" loading={isLoading}>
+                        <Form.Group widths="equal">
+                            <Form.Input
+                                fluid
+                                label="Price Name"
+                                placeholder="Price Name"
+                                value={price.name || ''}
+                                onChange={onPriceNameInputChange}
+                                required
+                            />
+                        </Form.Group>
+                        <FormFooter
+                            isSubmitDisabled={!isReadyToSubmit}
+                            onSubmitClick={savePrice}
+                            backButtonURL={ADMIN_APP_ROUTES.PRICES}
+                        />
+                    </Form>
+                </>
+            )}
+        </div>
+    );
+};
