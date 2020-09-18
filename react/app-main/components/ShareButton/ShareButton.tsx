14530e4e64011302ae0085957a871184070e69b9
lancelotimb <lancelot.imb@gmail.com>
2020-09-19 01:30:38 +0200

    Add share icon on copy button

--- react/app-main/components/ShareButton/ShareButton.tsx
+++ react/app-main/components/ShareButton/ShareButton.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as copyTextToClipboard from 'copy-text-to-clipboard';
 import { Button, ButtonVariant } from 'tmw-main/components/Button';
 import { useToastMessageContext } from 'tmw-main/components/ToastMessage';
+import { CopyIcon } from 'tmw-main/icons/CopyIcon';
 
 export const ShareButton: React.FunctionComponent = () => {
     const { openToastMessage } = useToastMessageContext();
@@ -12,6 +13,11 @@ export const ShareButton: React.FunctionComponent = () => {
     };
 
     return (
-        <Button content="Share" variant={ButtonVariant.WHITE_TEXT_BLACK} onClick={onButtonClick} />
+        <Button
+            content="Share"
+            variant={ButtonVariant.WHITE_TEXT_BLACK}
+            onClick={onButtonClick}
+            icon={CopyIcon}
+        />
     );
 };
