6e53d4967a41d3db44986d21811a997cc0cbddf7
Josselin Pennors <josselin.pennors@hotmail.fr>
2020-11-15 11:48:59 +0100

    fix config syntax issue

--- app/Services/MailSender.php
+++ app/Services/MailSender.php
@@ -21,7 +21,7 @@ class MailSender
     public static function send_resource_websites_availability_alert($unavailable_resources)
     {
         $subject = 'Tips My Web - Sites Web inaccessibles';
-        $receiver = config('mail.receiver.admin');
+        $receiver = config('mail.reveiver.admin');
 
         Mail::send('resource_websites_availability_alert', ['unavailable_resources' => $unavailable_resources], function($message) use ($subject, $receiver) {
             $message->to($receiver);
