69f53fa9ddad35536ae7be481e043eaaf46eba81
Josselin Pennors <josselin.pennors@hotmail.fr>
2021-02-01 21:50:36 +0100

    add google analytics

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,6 +7,17 @@
     <meta name="theme-color" content="#f46d7b">
     <title>TipsMyWeb</title>
     <link rel="icon" href="/images/logo-icon.png">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-97827898-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-97827898-2');
+    </script>
+
 </head>
 <body>
 	<div id="root"></div>
