56157c9f6c16504871a1642d3b642de1dfb6a10a
lancelotimb <lancelot.imb@gmail.com>
2021-04-21 10:02:20 +0200

    Fix copy-to-clipboard

--- frontend3/app-main/components/ShareButton/ShareButton.tsx
+++ frontend3/app-main/components/ShareButton/ShareButton.tsx
@@ -1,5 +1,5 @@
-// import * as copyTextToClipboard from 'copy-text-to-clipboard';
 import * as React from 'react';
+import { copyTextToClipboard } from 'tmw-common/utils/copy-text-to-clipboard';
 import { Button, ButtonVariant } from 'tmw-main/components/Button';
 import { ButtonProps } from 'tmw-main/components/Button/Button';
 import { useToastMessageContext } from 'tmw-main/components/ToastMessage';
@@ -14,7 +14,7 @@ export const ShareButton: React.FunctionComponent<ShareButtonProps> = ({ classNa
     const { openToastMessage } = useToastMessageContext();
 
     const onButtonClick = (): void => {
-        // copyTextToClipboard(window.location.href);
+        copyTextToClipboard(window.location.href);
         openToastMessage('URL was successfully copied in clipboard !');
     };
 
